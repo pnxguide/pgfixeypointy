d1144c7dff68c6a0498ee4111c6bc1f0b6c01760
pnxguide <yuttapichai.ke@gmail.com>
2023-04-19 03:10:48 +0000

    fixed output function

--- sql/test.sql
+++ sql/test.sql
@@ -1,10 +1,14 @@
 DROP EXTENSION IF EXISTS pgfixeypointy CASCADE;
+
 CREATE EXTENSION pgfixeypointy;
 
 DROP TABLE IF EXISTS test_fxypty;
+
 CREATE TABLE test_fxypty (
     x fxypty,
     y fxypty
 );
+
+INSERT INTO test_fxypty (x, y) VALUES ('10.123', '2.45');
 
 SELECT * FROM test_fxypty;
