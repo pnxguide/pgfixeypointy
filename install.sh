6a69717f8f8d5ec7513658346b5e9a3535e8b272
pnxguide <yuttapichai.ke@gmail.com>
2023-05-30 18:09:26 +0000

    now install.sh install libfixeypointy

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 make clean;
 sudo make install;
 
-# # Install libfixeypointy.so
-# cd third_party/libfixeypointy;
-# sh install.sh;
+# Install libfixeypointy.so
+cd third_party/libfixeypointy;
+sh install.sh;
